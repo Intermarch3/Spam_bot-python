13e3d9a656561d610e93b4c767cb03855f484473
Intermarch3 <78302154+Intermarch3@users.noreply.github.com>
2021-07-21 10:22:38 +0200

    Add files via upload

---
+++ SpamBot_txt_perso.py
@@ -0,0 +1,9 @@
+import pyautogui
+from time import sleep
+
+sleep(5)     # après avoir lancer le programme, tu a 5 seconde pour clické dans un espace de text et le spam commence
+nb = 0
+for nb in range(200):     # met entre parenthèse le nombre de fois à spam
+    pyautogui.typewrite("je suis le meilleur en spam")      # tape dans les guillemets le message à spam
+    pyautogui.press("enter")
+    nb = nb + 1
