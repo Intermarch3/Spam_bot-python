3a41097e31410bb013e632acb13d89f647a6b7b6
Intermarch3 <78302154+Intermarch3@users.noreply.github.com>
2021-07-21 10:24:54 +0200

    Update and rename SpamBot_txt_perso.py to SpamBot_msg_unique.py

--- SpamBot_msg_unique.py
+++ SpamBot_msg_unique.py
@@ -3,7 +3,7 @@ from time import sleep
 
 sleep(5)     # après avoir lancer le programme, tu a 5 seconde pour clické dans un espace de text et le spam commence
 nb = 0
-for nb in range(200):     # met entre parenthèse le nombre de fois à spam
+for nb in range(200):     # met entre parenthèse le nombre de fois à spam le message
     pyautogui.typewrite("je suis le meilleur en spam")      # tape dans les guillemets le message à spam
     pyautogui.press("enter")
     nb = nb + 1
