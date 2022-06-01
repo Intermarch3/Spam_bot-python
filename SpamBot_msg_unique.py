df00f2f9704d6d6ef80083537c4bfb2174560640
Intermarch3 <lucasleclerc74@gmail.com>
2022-06-01 08:41:59 +0200

    Update SpamBot_msg_unique.py

--- SpamBot_msg_unique.py
+++ SpamBot_msg_unique.py
@@ -3,8 +3,10 @@ from time import sleep
 
 msg = input("Tape le message a spam: ")
 nb = int(input("Combien de fois on le spam ?\n"))
+t = float(input("temps d'attente entre 2 msg (en s): "))
 
 sleep(5)     # après avoir lancer le programme, tu a 5 seconde pour clické dans un espace de text et le spam commence
 for i in range(nb):     
     pyautogui.typewrite(msg)      
     pyautogui.press("enter")
+    sleep(t)
