209adb1d9e52b6b8d336b41490b79a9752ff665d
Intermarch3 <lucasleclerc74@gmail.com>
2022-06-01 08:39:23 +0200

    Update SpamBot_msg_unique.py

--- SpamBot_msg_unique.py
+++ SpamBot_msg_unique.py
@@ -1,9 +1,10 @@
 import pyautogui
 from time import sleep
 
+msg = input("Tape le message a spam: ")
+nb = int(input("Combien de fois on le spam ?\n"))
+
 sleep(5)     # après avoir lancer le programme, tu a 5 seconde pour clické dans un espace de text et le spam commence
-nb = 0
-for nb in range(200):     # met entre parenthèse le nombre de fois à spam le message
-    pyautogui.typewrite("je suis le meilleur en spam")      # tape dans les guillemets le message à spam
+for i in range(nb):     
+    pyautogui.typewrite(msg)      
     pyautogui.press("enter")
-    nb = nb + 1
