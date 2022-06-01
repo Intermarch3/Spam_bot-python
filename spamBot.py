62fc5d37615d67749027c824dce485da62fb7429
Intermarch3 <lucasleclerc74@gmail.com>
2022-06-01 08:42:53 +0200

    Update spamBot.py

--- spamBot.py
+++ spamBot.py
@@ -1,9 +1,12 @@
 import pyautogui
 from time import sleep
 
+t = float(input("Temps d'attente entre 2 msg (en s): "))
+
 sleep(5)  # après avoir lancer le programme, tu a 5 seconde pour clické dans un espace de text et le spam commence
 f = open("txt_spamBot", 'r')  # envoye tout le contenu du fichier txt_spambot
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
+    sleep(t)
 
